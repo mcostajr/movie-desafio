95887751ab56bb3e1d1e471ff96097f4075a0238
mcostajr <maurofreirejr@gmail.com>
2021-12-06 03:29:39 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import * as React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from './router';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styles from './styles.module.scss';
+
+function Home() {
+	return (
+		<div className={styles.container}>
+            
+		</div>
+	);
+}
+export default Home
